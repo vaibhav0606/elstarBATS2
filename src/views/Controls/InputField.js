72963c89013c232e1fb6e0bbcfa09d9bbac8ccc5
Akash Uttam Gaikwad <97182740+akash1810gaikiwad@users.noreply.github.com>
2023-10-16 13:11:00 +0530

    AkashToday16/10/2023

--- src/views/Controls/InputField.js
+++ src/views/Controls/InputField.js
@@ -2,37 +2,35 @@ import { FormItem, Input } from 'components/ui'
 import { Field } from 'formik'
 import React, { useEffect } from 'react'
 
-const InputField = ({lable,placeholder,name,type,errors,touched,asterisk}) => {
-   
-//    useEffect(() => {
-//     const errorst = {name};
-//    console.log(name);
-//   alert(name)
-//    }, [])
-   
-  return (
-    <FormItem
-    asterisk={asterisk}
-        label={lable}
-        errorMessage={
-            <p className="text-xs italic">
-                {errors.name}
-            </p>
-        }
-        invalid={
-            errors.name && touched.name
-        }
-    >
-        <Field
-            size="sm"
-            type={type}
-            autoComplete="off"
-            name={name}
-            placeholder={placeholder}
-            component={Input}
-        />
-    </FormItem>
-  )
+const InputField = ({
+    lable,
+    placeholder,
+    name,
+    type,
+    errors,
+    touched,
+    asterisk,
+    category,
+}) => {
+    return category === 'text' ? (
+        <>
+            <FormItem
+                asterisk={asterisk}
+                label={lable}
+                errorMessage={<p className="text-xs italic">{errors.name}</p>}
+                invalid={errors.name && touched.name}
+            >
+                <Field
+                    size="sm"
+                    type={type}
+                    autoComplete="off"
+                    name={name}
+                    placeholder={placeholder}
+                    component={Input}
+                />
+            </FormItem>
+        </>
+    ) : null
 }
 
 export default InputField
